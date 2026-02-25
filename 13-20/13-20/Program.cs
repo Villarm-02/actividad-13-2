c3fa30598be64675b7dd767bce3bd07b6077e4ad
Villarm-02 <dv2555782@gmail.com>
2026-02-24 20:19:14 -0600

    cuantos números son positivos

---
+++ 13-20/13-20/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("¿Cuántos números?: ");
+int n = int.Parse(Console.ReadLine());
+int positivos = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("Ingrese un número: ");
+    int num = int.Parse(Console.ReadLine());
+    if (num > 0)
+    { 
+    positivos += 1;
+    }
+}
+
+Console.WriteLine("Positivos: " + positivos);

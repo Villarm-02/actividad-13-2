5c657e96771cd42d0162aa97b69aedc651e308e1
Villarm-02 <dv2555782@gmail.com>
2026-02-24 20:21:31 -0600

    cuantos son negativos

---
+++ 13-21/13-21/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("¿Cuántos números?: ");
+int n = int.Parse(Console.ReadLine());
+int negativos = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("Ingrese un número: ");
+    int num = int.Parse(Console.ReadLine());
+    if (num < 0)
+        negativos+=1;
+}
+
+Console.WriteLine("Negativos: " + negativos);

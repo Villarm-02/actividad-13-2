4b5fff72f52179fa30efcdd6ff4ff957b85660b4
Villarm-02 <dv2555782@gmail.com>
2026-02-24 20:10:53 -0600

    cual es el mayor de 5

---
+++ 13-18/13-18/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Ingrese número: ");
+int mayor = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= 5; i++)
+{
+    Console.Write("Ingrese número: ");
+    int num = int.Parse(Console.ReadLine());
+
+    if (num > mayor)
+    {
+        mayor = num;
+    }
+}
+
+Console.WriteLine($"Mayor: {mayor}");

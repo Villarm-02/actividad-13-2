21984b7573f85f57922da94e40f01d8a6e8766c7
Villarm-02 <dv2555782@gmail.com>
2026-02-24 20:13:42 -0600

    cual es menor en 5

---
+++ 13-19/13-19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Ingrese número: ");
+int menor = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= 5; i++)
+{
+    Console.Write("Ingrese número: ");
+    int num = int.Parse(Console.ReadLine());
+
+    if (num < menor)
+    {
+        menor = num;
+    }
+}
+
+Console.WriteLine($"Menor: {menor}");

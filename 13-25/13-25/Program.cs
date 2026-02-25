1158e5c913ef581eff8cfd46cb61f11d58be67f6
Villarm-02 <dv2555782@gmail.com>
2026-02-24 20:55:13 -0600

    escalera de asteriscos

---
+++ 13-25/13-25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Ingrese la altura de la escalera: ");
+int altura = int.Parse(Console.ReadLine());
+for (int i = 1; i <= altura; i++)
+{
+    for (int d = 1; d<= i;d++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}

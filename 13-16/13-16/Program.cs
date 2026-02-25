3cb24d52f24c1a87220665f7f2ae4db3b7c519b0
Villarm-02 <dv2555782@gmail.com>
2026-02-24 20:02:55 -0600

    cuantos son mayores a 10

---
+++ 13-16/13-16/Program.cs
@@ -0,0 +1,12 @@
+﻿int contador = 0;
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write("Ingrese número: ");
+    int num = int.Parse(Console.ReadLine());
+
+    if (num > 10)
+        contador += 1;
+}
+
+Console.WriteLine("Mayores que 10: " + contador);

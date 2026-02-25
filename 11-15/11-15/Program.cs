576c02aa35fc7405aab23f28843ff36c00de3803
Villarm-02 <dv2555782@gmail.com>
2026-02-24 19:50:38 -0600

    promedio de 5 números

---
+++ 11-15/11-15/Program.cs
@@ -0,0 +1,9 @@
+﻿double suma = 0;
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write("Ingrese un número: ");
+    double num = double.Parse(Console.ReadLine());
+    suma += num;
+}
+double promedio = suma / 5;
+Console.WriteLine($"Su promedio es: {promedio}");

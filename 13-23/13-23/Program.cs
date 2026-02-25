579969fa66a637c054ab71db25fb911354f84085
Villarm-02 <dv2555782@gmail.com>
2026-02-24 20:28:30 -0600

    promedio de x notas

---
+++ 13-23/13-23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Cantidad de notas: ");
+int n = int.Parse(Console.ReadLine());
+double suma = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("Ingrese la nota: ");
+    suma += double.Parse(Console.ReadLine());
+}
+
+double promedio = suma / n;
+Console.WriteLine("Promedio: " + promedio);

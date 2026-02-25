df820e6ef29f1c545fbfe93ad1f10df1a93dc269
Villarm-02 <dv2555782@gmail.com>
2026-02-24 20:26:07 -0600

    total vendido

---
+++ 13-22/13-22/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Cantidad de ventas: ");
+int n = int.Parse(Console.ReadLine());
+double total = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("De cuento fue la venta: ");
+    total += double.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Total vendido: " + total);

cde2a7540d3a9fdb8925d3640d321379795b4b81
Villarm-02 <dv2555782@gmail.com>
2026-02-24 20:34:19 -0600

    promedio y clasificar "Aprobo" o "Reprobo"

---
+++ 13-24/13-24/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Cantidad de notas: ");
+int n = int.Parse(Console.ReadLine());
+double suma = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("Ingrese la nota: ");
+    suma += double.Parse(Console.ReadLine());
+}
+
+double promedio = suma / n;
+
+if (promedio >= 61)
+{
+    Console.WriteLine("Aprobado con: " + promedio);
+}
+else
+{
+    Console.WriteLine("Reprobado con: " + promedio);
+}

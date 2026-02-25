7e9cc3e35a57ca981955c50f614d5c12f0c4b999
Villarm-02 <dv2555782@gmail.com>
2026-02-24 19:44:59 -0600

    suma 3 digitos

---
+++ 13-14/13-14/Program.cs
@@ -0,0 +1,8 @@
+﻿int suma = 0;
+for (int i = 1; i <= 3; i++)
+{
+    Console.Write("Ingrese un número: ");
+    int num = int.Parse(Console.ReadLine());
+    suma += num;
+}
+Console.WriteLine(suma);
